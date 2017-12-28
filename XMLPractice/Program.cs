8275d46c98da7aa648b83a168770a5851220b14d
Nurkaiyr <yedige21@gmail.com>
2017-12-28 13:57:17 +0600

    Practice

---
+++ XMLPractice/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XMLPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XMLDocument.Print("Book.xml");
+        }
+    }
+}
---
+++ XMLPractice/XMLDocument.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace XMLPractice
+{
+    public class XMLDocument
+    {
+        public static void Print(string path)
+        {
+            XmlTextReader xmlReader = new XmlTextReader(path);
+
+            while (xmlReader.Read())
+            {
+                if (xmlReader.NodeType == XmlNodeType.Element) Console.Write(xmlReader.Name + "  ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
